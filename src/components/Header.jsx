fc6f4c9507e7dfc99a83e3b00ec3cf6c35ffd9a2
amithkashyap <amithkashyaprv@gmail.com>
2024-02-12 19:46:27 +0530

    added Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "../logo.svg";
+
+const Header = () => {
+  return (
+    <div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>THE REACT QUIZ</h2>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
